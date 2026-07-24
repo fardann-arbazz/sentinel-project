92e44ee2861243e62609121ad6ca191af43a487c
fardann-arbazz <arbasfardan@gmail.com>
2026-07-24 16:06:45 +0700

    first commit

---
+++ contracts/src/SentinelPolicy.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.20;
+
+contract SentinelPolicy {
+    struct Policy {
+        bool blockUnlimitedApproval;
+        bool warnUknownContract;
+    }
+
+    
+}
